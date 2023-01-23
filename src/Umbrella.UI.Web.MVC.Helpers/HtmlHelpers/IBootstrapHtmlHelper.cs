f01a9c1286ef02f05284138903dd1ad09c5acf73
Francesco Garavaglia <Francesco.Garavaglia@outlook.it>
2023-01-23 23:48:16 +0100

    first commit

---
+++ src/Umbrella.UI.Web.MVC.Helpers/HtmlHelpers/IBootstrapHtmlHelper.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+
+namespace Umbrella.UI.Web.MVC.Helpers.HtmlHelpers
+{
+    /// <summary>
+    /// Abstraction of Bootstrp component helper
+    /// </summary>
+    public interface IBootstrapHtmlHelper
+    {
+        /// <summary>
+        /// BUdile the HTML for the specific control
+        /// </summary>
+        /// <param name="ctrlId"></param>
+        /// <returns></returns>
+        string BuildHtml(string ctrlId);
+        /// <summary>
+        /// Renders the Html on MVC Views
+        /// </summary>
+        /// <param name="ctrlId"></param>
+        /// <returns></returns>
+        IHtmlContent Render(string ctrlId = "");
+
+    }
+}
